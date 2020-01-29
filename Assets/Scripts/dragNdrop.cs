cbb9c30d8f1bd41956690db0e61acc0b8fc6bc51
Vanja <vv52@humboldt.edu>
2020-01-29 13:49:19 -0800

    implemented basic drag n drop script, beginning to reshape playfield for better space efficiency and clarity of gameplay zone.

---
+++ Assets/Scripts/dragNdrop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragNdrop : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector2 startPosition;
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public void StartDrag()
+    {
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+    }
+}
--- Assets/Scripts/draw_cards.cs
+++ Assets/Scripts/draw_cards.cs
@@ -9,17 +9,23 @@ public class draw_cards : MonoBehaviour
     public GameObject PlayerArea;
     public GameObject OpponentArea;
 
+    List<GameObject> cards = new List<GameObject>();
+
     void Start()
     {
-        
+        cards.Add(Card1);
+        cards.Add(Card2);
     }
 
     public void OnClick()
     {
         for (var i = 0; i < 5; i++)
         {
-            GameObject playerCard = Instantiate(Card1, new Vector3(0, 0, 0), Quaternion.identity);
+            GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
             playerCard.transform.SetParent(PlayerArea.transform, false);
+
+            GameObject opponentCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+            opponentCard.transform.SetParent(OpponentArea.transform, false);
         }
     }
 }
