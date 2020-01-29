f5cb337f9ec5b424806890e87d47f1c946be2fc6
Vanja <vv52@humboldt.edu>
2020-01-29 12:50:29 -0800

    basic assets added, draw cards script partially implemented

---
+++ Assets/Scripts/draw_cards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class draw_cards : MonoBehaviour
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject OpponentArea;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            GameObject playerCard = Instantiate(Card1, new Vector3(0, 0, 0), Quaternion.identity);
+            playerCard.transform.SetParent(PlayerArea.transform, false);
+        }
+    }
+}
